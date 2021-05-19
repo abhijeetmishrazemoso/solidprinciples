311dbd55c2c5708b7c075913078be7066e272f13
abhijeetmishrazemoso <84129520+abhijeetmishrazemoso@users.noreply.github.com>
2021-05-19 10:01:39 +0530

    Update Vaccinator.java

--- src/main/java/contracts/Vaccinator.java
+++ src/main/java/contracts/Vaccinator.java
@@ -2,8 +2,7 @@ package contracts;
 
 import equipment.Vaccine;
 import high.*;
-//violating open/Closed Principle. extensions cannot modify this code.
+
 public interface Vaccinator extends Available {
     boolean isVaccinationSuccessful(Vaccine vaccine, Patient patient);
-    //
 }
